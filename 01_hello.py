c101fc5fb7803f2e69a5356787f45fbe69d54baa
Selvamanan R <84980271+selvalr@users.noreply.github.com>
2026-01-28 08:43:26 +0000

    hello python

--- 01_hello.py
+++ 01_hello.py
@@ -1,10 +1,10 @@
-print(2 + 3)             # addition(+)
-print(3 - 1)             # subtraction(-)
-print(2 * 3)             # multiplication(*)
-print(3 / 2)             # division(/)
-print(3 ** 2)            # exponential(**)
-print(3 % 2)             # modulus(%)
-print(3 // 2)            # Floor division operator(//)
+print(3 + 4)             # addition(+)
+print(3 - 4)             # subtraction(-)
+print(3 * 4)             # multiplication(*)
+print(3 / 4)             # division(/)
+print(3 ** 4)            # exponential(**)
+print(3 % 4)             # modulus(%)
+print(3 // 4)            # Floor division operator(//)
 
 
 # Checking data types
